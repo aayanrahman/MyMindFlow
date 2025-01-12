f4ec450b5b7ad56b6f969234414361536cc0f195
Jason <jasonathena0604@gmail.com>
2025-01-12 06:44:58 -0500

    done deal

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/sidebar/sidebar';
 import Journal from './components/journal/Journal';
-import UserChats from './components/userchats/userChats';
+import UserChats from './components/userchats/UserChats';
+
 
 function App() {
   return (
